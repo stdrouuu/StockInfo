3720494f20f96a4dd6d1e4ad3fd1129f25fbf727
Dev <dev@example.com>
2026-06-10 22:28:53 +0700

    delete

--- resources/views/produk/produk.blade.php
+++ resources/views/produk/produk.blade.php
@@ -521,7 +521,7 @@
             </div>
             <div class="space-y-2">
                 <label class="text-[10px] font-black text-slate-800 uppercase tracking-wider">Jumlah Produk (Stok)</label>
-                <input type="number" name="stok" x-model="stok" readonly class="w-full px-4 py-3 bg-slate-100 border border-slate-200 rounded-xl text-sm text-slate-400 cursor-not-allowed focus:outline-none transition-all">
+                <input type="number" name="stok" x-model="stok" class="w-full px-4 py-3 bg-slate-50 border border-slate-200 rounded-xl text-sm text-slate-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all">
             </div>
 
             <div class="space-y-2">
